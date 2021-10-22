a2003ad4ad7ca16710558d3cf0d18e51358f369a
bramboom <bramboom1@yandex.ru>
2021-10-22 16:45:53 +0700

    Добавлены классы Note, NoteCategory

---
+++ WindowsFormsApp1/WindowsFormsApp1/NoteCategory.cs
@@ -0,0 +1,16 @@
+﻿namespace NoteApp
+{
+    /// <summary>
+    /// Категории заметок по их содержанию
+    /// </summary>
+    public enum NoteCategory
+    {
+        Job,
+        Home,
+        HealthAndSports,
+        People,
+        Documents,
+        Finance,
+        Other
+    }
+}
