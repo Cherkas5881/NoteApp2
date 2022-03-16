03d541edf2485961e7fb7007405f58efb2360723
bramboom <bramboom@yandex.ru>
2022-03-16 08:11:57 +0700

    add NoteForm

---
+++ NoteApp/NoteAppUI/NoteForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace NoteAppUI
+{
+    partial class NoteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TitleBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CategoryComboBox = new System.Windows.Forms.ComboBox();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.TimeCreated = new System.Windows.Forms.DateTimePicker();
+            this.TimeChanged = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // TitleBox
+            // 
+            this.TitleBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TitleBox.Location = new System.Drawing.Point(90, 12);
+            this.TitleBox.Name = "TitleBox";
+            this.TitleBox.Size = new System.Drawing.Size(580, 22);
+            this.TitleBox.TabIndex = 0;
+            this.TitleBox.TextChanged += new System.EventHandler(this.TitleBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Category:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Created:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Modified:";
+            // 
+            // CategoryComboBox
+            // 
+            this.CategoryComboBox.FormattingEnabled = true;
+            this.CategoryComboBox.Location = new System.Drawing.Point(90, 40);
+            this.CategoryComboBox.Name = "CategoryComboBox";
+            this.CategoryComboBox.Size = new System.Drawing.Size(208, 24);
+            this.CategoryComboBox.TabIndex = 5;
+            this.CategoryComboBox.SelectedIndexChanged += new System.EventHandler(this.CategoryComboBox_SelectedIndexChanged);
+            // 
+            // OKButton
+            // 
+            this.OKButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.OKButton.Location = new System.Drawing.Point(484, 495);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(90, 30);
+            this.OKButton.TabIndex = 8;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CancelButton.Location = new System.Drawing.Point(580, 495);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(90, 30);
+            this.CancelButton.TabIndex = 9;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TextBox.Location = new System.Drawing.Point(12, 130);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBox.Size = new System.Drawing.Size(658, 359);
+            this.TextBox.TabIndex = 10;
+            this.TextBox.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // TimeCreated
+            // 
+            this.TimeCreated.Enabled = false;
+            this.TimeCreated.Location = new System.Drawing.Point(90, 70);
+            this.TimeCreated.Name = "TimeCreated";
+            this.TimeCreated.Size = new System.Drawing.Size(208, 22);
+            this.TimeCreated.TabIndex = 11;
+            // 
+            // TimeChanged
+            // 
+            this.TimeChanged.Enabled = false;
+            this.TimeChanged.Location = new System.Drawing.Point(90, 98);
+            this.TimeChanged.Name = "TimeChanged";
+            this.TimeChanged.Size = new System.Drawing.Size(208, 22);
+            this.TimeChanged.TabIndex = 12;
+            // 
+            // NoteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 532);
+            this.Controls.Add(this.TimeChanged);
+            this.Controls.Add(this.TimeCreated);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.CategoryComboBox);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.TitleBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "NoteForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add/Edit Note";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TitleBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox CategoryComboBox;
+        private System.Windows.Forms.Button OKButton;
+        private new System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.DateTimePicker TimeCreated;
+        private System.Windows.Forms.DateTimePicker TimeChanged;
+    }
+}
---
+++ NoteApp/NoteAppUI/NoteForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using NoteApp;
+
+namespace NoteAppUI
+{
+    /// <summary>
+    /// Форма NoteForm, выполняющая редактирование и добавление заметки
+    /// </summary>
+    public partial class NoteForm : Form
+    {
+        /// <summary>
+        /// Создание локальной переменной типа Note
+        /// Исопльзуется для создания и изменения заметки
+        /// </summary>
+        private Note _note = new Note();
+
+        /// <summary>
+        /// Создание доступной для другой формы переменной типа Note
+        /// </summary>
+        public Note Note
+        {
+            get => _note;
+            set
+            {
+                _note = (Note)value.Clone();
+                DisplayNote();
+            }
+        }
+
+        /// <summary>
+        /// Загрузка формы NoteForm
+        /// </summary>
+        public NoteForm()
+        {
+            InitializeComponent();
+
+            //Источником данных для списка является класс-перечисление "Категория заметки"
+            CategoryComboBox.DataSource = Enum.GetValues(typeof(NoteCategory));
+        }
+
+        /// <summary>
+        /// Реализация отображения содержимого формы EditNote
+        /// в зависимости от содержимого переданной заметки:
+        /// либо для редактирования, либо создание новой
+        /// </summary>
+        public void DisplayNote()
+        {
+            //Обработка передачи пустой заметки для ее создания
+            if (_note == null)
+            {
+                TimeCreated.Text = DateTime.Now.ToShortDateString();
+                TimeChanged.Text = DateTime.Now.ToShortDateString();
+                TitleBox.Text = @"Без названия";
+                return;
+            }
+
+            //Обработка передачи заметки для редактирования
+            TitleBox.Text = _note.Title;
+            TextBox.Text = _note.Text;
+            CategoryComboBox.SelectedItem = _note.Category;
+            TimeCreated.Text = _note.ToFormattedTime(_note.IsCreated);
+            TimeChanged.Text = _note.ToFormattedTime(_note.IsChanged);
+        }
+
+        /// <summary>
+        /// Изменения содержимого TitleBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TitleBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                _note.Title = TitleBox.Text;
+                TitleBox.BackColor = Color.White;
+            }
+            catch
+            {
+                //Обработка исключения, если длина названия больше 50 символов
+                TitleBox.BackColor = Color.LightPink;
+            }
+        }
+
+        /// <summary>
+        /// Изменене выбранного элемента CategoryComboBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CategoryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _note.Category = (NoteCategory)CategoryComboBox.SelectedItem;
+        }
+
+        /// <summary>
+        /// Изменения содержимого TextBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            _note.Text = TextBox.Text;
+        }
+
+        /// <summary>
+        /// Обработка события нажатия на кнопку "ОК"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _note.Title = TitleBox.Text;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                return;
+            }
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        /// <summary>
+        /// Обработка события нажатия на кнопку "Cancel"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
