18724aaee8ad7f3262aa5c9b9beb8eb3657ff9ad
bramboom <bramboom@yandex.ru>
2022-03-16 08:10:55 +0700

    add EditForm

---
+++ NoteApp/NoteAppUI/AboutForm.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace NoteAppUI
+{
+    /// <summary>
+    /// Форма AboutForm, содержащая информацию о разработчике
+    /// </summary>
+    public partial class AboutForm : Form
+    {
+        /// <summary>
+        /// Загрузка формы AboutForm
+        /// </summary>
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Обрабокта события нажатия на ссылку GitHub
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void linkLabel2_MouseClick(object sender, MouseEventArgs e)
+        {
+            Process.Start(linkLabel2.Text);
+        }
+
+        /// <summary>
+        /// Обработка события нажатия на ссылку
+        /// на почтовый ящик
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("mailto: alicewhite00@mail.ru");
+        }
+    }
+}
